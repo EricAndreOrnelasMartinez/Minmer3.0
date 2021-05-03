1644d3caf126352cd0ddb36722cbe26efbecf192
Your Name <you@example.com>
2021-05-03 09:32:11 -0500

    add

--- tables/index.php
+++ tables/index.php
@@ -12,7 +12,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/cssforcitys.css">
-    <title>CDMX</title>
+    <title id="title"><?php $city  ?></title>
+    <script>
+        const title = document.getElementById('title')
+        title.InnerHTML = title.InnerHTML.toUpperCase()
+    </script>
 </head>
 <body>
     <header>
