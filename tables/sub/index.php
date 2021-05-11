7b6c3b3d9d030dd0f8242eae682289605a9cd41d
Your Name <you@example.com>
2021-05-11 08:50:58 -0500

    add

--- tables/sub/index.php
+++ tables/sub/index.php
@@ -164,7 +164,7 @@ $id = $_GET['id'];
             $aux = $_SESSION['nivel'];
             if($aux == 5 || $aux == 6){ 
             ?>
-             <td><a href="./Editar/?id=<?php echo $show['ID_SQL']; ?>&city=<?php echo $city ?>&idu=<?php echo $show['ID_sub'] ?>"><button type="button" class="btn btn-succes">Modificar</button></a></td>
+             <td><a href="./Edit/?id=<?php echo $show['ID_SQL']; ?>&city=<?php echo $city ?>&idu=<?php echo $show['ID_sub'] ?>"><button type="button" class="btn btn-succes">Modificar</button></a></td>
              <td><a href="../PHP/delete.php?id=<?php echo $show['ID_SQL'] ?>&city=<?php echo $city ?>"><button type="button">Eliminar</button></a></td>
              <td><a href="../PHP/terminar.php?id=<?php echo $show['ID_SQL'] ?>&city=<?php echo $city ?>"><button type="button">Terminar</button></a></td>
             <?php
