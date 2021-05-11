649a5d4df82be69e1daddd756559284733a755e5
Your Name <you@example.com>
2021-05-11 12:25:49 -0500

    add

--- PHP/delete.php
+++ PHP/delete.php
@@ -2,7 +2,7 @@
 require_once('dbcon.php');
 $id = $_POST['id'];
 $city = $_POST['city'];
-$sql = 'DELETE FROM $city WHERE ID_SQL=$id'; 
+$sql = "DELETE FROM $city WHERE ID_SQL=$id"; 
 $res = mysqli_query($con, $sql);
 if($res){
     echo json_encode('1');
