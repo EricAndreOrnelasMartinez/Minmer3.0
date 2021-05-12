a140690a9ec57a343e33c9d583e9ef5692c042b5
Your Name <you@example.com>
2021-05-11 20:59:15 -0500

    add

--- PHP/finish.php
+++ PHP/finish.php
@@ -4,7 +4,7 @@ $id = $_POST['id'];
 $total = $_POST['total'];
 $city = $_POST['city'];
 $sql = "UPDATE $city SET Terminado=1 WHERE ID_SQL=$id";
-if($total === 97){
+if($total === '97'){
     $resSQL = mysqli_query($con, $sql);
     if($resSQL){
         echo json_encode('1');
