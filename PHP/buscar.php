2d907e40f5063fc5e3d18c90a654bff1fe66481f
Your Name <you@example.com>
2021-05-13 08:48:56 -0500

    add

--- PHP/buscar.php
+++ PHP/buscar.php
@@ -4,15 +4,14 @@ $city = $_POST['city'];
 $artibute = $_POST['atribute'];
 $query = $_POST['query'];
 $query2 = $_POST['query2'];
-echo json_encode("todo bien con '$query2' y con '$query'");
     if($artibute === 'FechaC' || $artibute === 'FechaE'){
         $sql = "SELECT * FROM  $city $artibute BETWEEN '$query' AND '$query2'";
         $resSQL = mysqli_query($con, $sql);
-        //echo json_encode(mysqli_fetch_all($ans, MYSQLI_ASSOC));
+        echo json_encode(mysqli_fetch_all($resSQL, MYSQLI_ASSOC));
     }else{
         $sql = "SELECT * FROM $city WHERE $artibute='$query'";
         $ans = mysqli_query($con, $sql);
-        //echo json_encode(mysqli_fetch_all($ans, MYSQLI_ASSOC));
+        echo json_encode(mysqli_fetch_all($ans, MYSQLI_ASSOC));
     }
 
 ?>
