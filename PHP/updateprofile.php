dc4dc1ba8dbf837f8bd15d3b110f4abb2017b636
Your Name <you@example.com>
2021-05-14 17:41:30 -0500

    add

--- PHP/updateprofile.php
+++ PHP/updateprofile.php
@@ -6,8 +6,7 @@ $mail = $_POST['mail'];
 $name = $_POST['nombre'];
 $last = $_POST['apellido'];
 $rowN = $_POST['rowN'];
-$nivel = $_POST['nivel'];
-$sql = "UPDATE users SET Mail='$mail',Nombre='$name',Apellido='$last',rowN=$rowN,Nivel=$nivel WHERE Mail='$mailB'";
+$sql = "UPDATE users SET Mail='$mail',Nombre='$name',Apellido='$last',rowN=$rowN WHERE Mail='$mailB'";
 $res = mysqli_query($con, $sql);
 if($res){
     $_SESSION['mail'] = $mail;
