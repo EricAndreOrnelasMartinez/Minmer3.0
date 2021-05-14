edc1dc6221ce614f0df50ce82c48c5d0799a6c82
Your Name <you@example.com>
2021-05-14 12:46:59 -0500

    add

--- Perfil/perfil.js
+++ Perfil/perfil.js
@@ -4,12 +4,25 @@ fetch('../PHP/getprofile.php')
 .then(dataF =>{
     for(i in dataF){
         let output = `
-        <input type="text" value="${dataF[i].Mail}"/>
-        <input type="text" value="${dataF[i].Nombre}"/>
-        <input type="text" value="${dataF[i].Apellido}"/>
-        <input type="number" value="${dataF[i].rowN}"/>
+        <input type="text" value="${dataF[i].Mail}" name="mail"/>
+        <input type="text" value="${dataF[i].Nombre} name="nombre""/>
+        <input type="text" value="${dataF[i].Apellido} name="apellido"/>
+        <input type="number" value="${dataF[i].rowN}" name="rowN"/>
         <input type="submit" value="Actualizar"/>
         `
         form.innerHTML = output
     }
+})
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    let data = new FormData(form)
+    fetch('../PHP/updateprofile.php', {
+        method: 'POST',
+        body: data
+    })
+    .then(res => res.json())
+    .then(dataF =>{
+        console.log(dataF)
+    })
 })
