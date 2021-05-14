937d3b258477d747492f2e4ab2685a8c17f809c1
Your Name <you@example.com>
2021-05-14 11:34:17 -0500

    add

--- Perfil/perfil.js
+++ Perfil/perfil.js
@@ -2,5 +2,14 @@ const form = document.getElementById('main')
 fetch('../PHP/getprofile.php')
 .then(res => res.json())
 .then(dataF =>{
-    console.log(dataF)
+    for(i in dataF){
+        let output = `
+        <input type="text" value="${dataF[i].Mail}"/>
+        <input type="text" value="${dataF[i].Nombre}"/>
+        <input type="text" value="${dataF[i].Apellido}"/>
+        <input type="number" value="${dataF[i].rowN}"/>
+        <input type="submit" value="Actualizar"/>
+        `
+        
+    }
 })
