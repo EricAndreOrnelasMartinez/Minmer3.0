3b1cd1415de4371151b4bf636b896bb29a2e7255
Your Name <you@example.com>
2021-05-14 08:27:01 -0500

    add

--- Modificaciones/index.php
+++ Modificaciones/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/cssforcitys.css">
+    <link rel="stylesheet" href="../CSS/cssforcitis.css">
     <title>Modificaciones</title>
     <?php  
     error_reporting(E_ALL);
