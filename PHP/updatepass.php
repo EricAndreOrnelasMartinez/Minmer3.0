a6df3406f540cdc73ea281a18904c6aba3138537
Your Name <you@example.com>
2021-05-14 21:45:25 -0500

    add

--- PHP/updatepass.php
+++ PHP/updatepass.php
@@ -5,7 +5,7 @@ $mail = $_SESSION['mail'];
 $now = $_POST['now'];
 $new = $_POST['new'];
 $nowpass = "SELECT Contrasena FROM users WHERE Mail='$mail'";
-$resnowpass = mysqli_query($con, $now);
+$resnowpass = mysqli_query($con, $nowpass);
 if($now === implode(mysqli_fetch_assoc($resnowpass))){
     $sql = "UPDATE users SET Contrasena='$new' WHERE Mail='$mail'";
     $updres = mysqli_query($con,$sql);
